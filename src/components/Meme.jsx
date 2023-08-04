64da7a644afa1199a1ca828abeb191d89810ad1c
Luka Mendonça <lukamendoncadev@proton.me>
2023-08-04 16:23:46 -0300

    setting Meme component body

---
+++ src/components/Meme.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Meme() {
+    return (
+        <form action="#">
+            <div className="input-group">
+                <input type="text" placeholder='Type upper text' />
+                <input type="text" placeholder='Type lower text' />
+            </div>
+            <button>Get a new meme image</button>
+        </form>
+    )
+}
