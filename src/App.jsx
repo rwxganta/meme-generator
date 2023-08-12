7ff82d08323f59fadadacae45df287e427403761
Luka Mendonça <lukamendoncadev@proton.me>
2023-08-12 16:06:01 -0300

    Studies on useState() - [arr] and {obj} + some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
+import Meme from './components/Meme';
 import './App.css';
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <div className="wrapper">
+          <Meme />
+        </div>
+      </main>
     </>
   )
 }
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import memesData from '../assets/memesData.js'
 
 export default function Meme() {
+    //const [meme, setMeme] = useState('');
+    const [allMemesData, setAllMemesData] = useState(memesData);
+    const [meme, setMeme] = useState({
+        topText: '',
+        bottomText: '',
+        randomImage: 'http://i.imgflip.com/1bij.jpg',
+    });
+
+
+    function getMemeImage() {
+        const memesArr = allMemesData.data.memes;
+        const random = Math.floor(Math.random() * memesArr.length);
+        const link = memesArr[random].url;
+        setMeme((prevState) => {
+            return {
+                ...prevState,
+                randomImage: link,
+            }
+        });
+    }
+
     return (
-        <form action="#">
-            <div className="input-group">
-                <input type="text" placeholder='Type upper text' />
-                <input type="text" placeholder='Type lower text' />
-            </div>
-            <button type='button'>Get a new meme image</button>
-        </form>
+        <>
+            <form action="#">
+                <div className="input-group">
+                    <input type="text" placeholder='Type upper text' />
+                    <input type="text" placeholder='Type lower text' />
+                </div>
+                <button type='button' onClick={getMemeImage}>Get a new meme image</button>
+            </form>
+            <img className='meme-image' src={meme.randomImage} alt="" />
+        </>
     )
 }
